d08588516acb63ba0e2738d9c29eb4d721fe13bf
QazCetelic <dev@malplena.net>
2025-07-27 20:40:48 +0200

    Adapt shell script for Docker to temporary directory changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,25 +1,8 @@
 #!/bin/sh
 
-if [ -z "$GIO_BASE_URL" ]; then
-  echo "Error: GIO_BASE_URL is not set."
-  exit 1
-fi
-
-if [ -z "$GIO_TEMPORARY_DIRECTORY" ]; then
-  echo "Error: GIO_TEMPORARY_DIRECTORY is not set."
-  exit 1
-fi
-
-if [ -z "$GIO_API_TOKEN" ]; then
-  echo "Error: GIO_API_TOKEN is not set."
-  exit 1
-fi
-
 interval=${GIO_INTERVAL:-300}
-tmp_dir=${GIO_TEMPORARY_DIRECTORY:-"/tmp/images"}
-
 while true; do
-  ./grist-image-optimizer --dir "$tmp_dir"
+  ./grist-image-optimizer
   echo "Waiting for $interval seconds..."
   sleep "$interval"
 done
